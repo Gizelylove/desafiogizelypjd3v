e2056b5258c4348b147f8df2eb05032b68d2221f
Gizely Duarte <45357020+Gizelylove@users.noreply.github.com>
2019-05-30 13:27:19 -0300

    Add files via upload

---
+++ jogogizelypjd3v/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1559233515,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/arrowupbutton-sheet0.png",
+		"images/arrowdownbutton-sheet0.png",
+		"images/arrowrightbutton-sheet0.png",
+		"images/arrowleftbutton-sheet0.png",
+		"images/backgoundanimed-sheet0.png",
+		"images/backgoundanimed-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/enemycontroller-sheet0.png",
+		"images/bottondestroyer-sheet0.png",
+		"images/enegyframeui-sheet0.png",
+		"images/enegyui-sheet0.png",
+		"images/tiledbackground.png",
+		"images/powerupcontroller-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
